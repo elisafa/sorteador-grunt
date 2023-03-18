e324573b1bde434bc8659dc46da48353e202ada0
Elisafa <elisafajunior@gmail.com>
2023-03-18 13:31:28 -0300

    instalacao e configuracao do plugin para minificar o html

--- gruntfile.js
+++ gruntfile.js
@@ -16,18 +16,18 @@ module.exports = function (grunt) {
         }
       }
     },
-    watch:{
-      less:{
+    watch: {
+      less: {
         files: ['src/styles/**/*.less'],
         tasks: ['less:development']
       }
     },
-    replace:{
-      dev:{
-        options:{
+    replace: {
+      dev: {
+        options: {
           patterns: [{
             match: 'ENDERECO_DO_CSS',
-            replacement: './style/main.css'
+            replacement: './styles/main.css'
           }]
         },
         files: [{
@@ -36,13 +36,41 @@ module.exports = function (grunt) {
           src: ['src/index.html'],
           dest: 'dev/'
         }]
+      },
+      dist: {
+        options: {
+          patterns: [{
+            match: 'ENDERECO_DO_CSS',
+            replacement: './styles/main.min.css'
+          }]
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['prebuild/index.html'],
+          dest: 'dist/'
+        }]
       }
-    }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'prebuild/index.html': 'src/index.html'
+        }
+      }
+    },
+    clean: [' prebuild']
   })
   grunt.loadNpmTasks('grunt-contrib-less')
   grunt.loadNpmTasks('grunt-contrib-watch')
   grunt.loadNpmTasks('grunt-replace')
+  grunt.loadNpmTasks('grunt-contrib-htmlmin')
+  grunt.loadNpmTasks('grunt-contrib-clean')
 
   grunt.registerTask('default', ['watch'])
-  grunt.registerTask('build', ['less:production'])
+  grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean'])
 }
